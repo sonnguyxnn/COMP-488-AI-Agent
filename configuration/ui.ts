2c46fbe1d71a19ef782de6f2fcbf971d6e6cd78e
hoangsonwwww <feronn2828@hotmail.com>
2025-02-25 16:59:54 -0500

    Fix: Customize chatbot UI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CHAT_HEADER: string = `Lumina AI - David Nguyen's Personal Assistant`;
+export const MESSAGE_PLACEHOLDER: string = `Chat with Lumina...`;
+export const FOOTER_MESSAGE: string = `Customized AI by David Nguyen @ UNC COMP-488`;
+export const CLEAR_BUTTON_TEXT: string = `Start New Conversation`;
+export const PAGE_TITLE: string = `David Nguyen's Personal Assistant`;
+export const PAGE_DESCRIPTION: string = `Chat with Lumina, David Nguyen's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
